859c7c702c8de8601693f2bf658c6c2d626dd6e8
andy-lee-cat <2048513251@qq.com>
2024-12-19 16:20:58 +0800

    Changes to be committed: modified:   03_Abstract_Factary/easy_solution.cpp modified:   README.md

--- 03_Abstract_Factary/easy_solution.cpp
+++ 03_Abstract_Factary/easy_solution.cpp
@@ -13,10 +13,11 @@ int main() {
         "classical"
     };
     std::cin >> n;
+    std::string s;
     for (int i = 0; i < n; i++) {
-        std::string s;
         std::cin >> s;
-        if (valid_types.find(s) != valid_types.end()) {
+        auto it = valid_types.find(s);
+        if (it != valid_types.end()) {
             for (auto furniture : furnitures) {
                 std::cout << s << " " << furniture << std::endl;
             }
