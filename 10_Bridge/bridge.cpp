25679653d61e1d0047cee9e1336ec20a50aa051e
andy-lee-cat <2048513251@qq.com>
2024-12-24 20:34:49 +0800

    Add 10_Bridge/

---
+++ 10_Bridge/bridge.cpp
@@ -0,0 +1,84 @@
+// bridge
+#include<iostream>
+#include<memory>
+
+class TV {
+public:
+    virtual void on() = 0;
+    virtual void off() = 0;
+    virtual void tuneChannel() = 0;
+};
+
+class SonyTV : public TV {
+public:
+    void on() override {
+        std::cout << "Sony TV is ON" << std::endl;
+    }
+    void off() override {
+        std::cout << "Sony TV is OFF" << std::endl;
+    }
+    void tuneChannel() override {
+        std::cout << "Switching Sony TV channel" << std::endl;
+    }
+};
+
+class TCLTV : public TV {
+public:
+    void on() override {
+        std::cout << "TCL TV is ON" << std::endl;
+    }
+    void off() override {
+        std::cout << "TCL TV is OFF" << std::endl;
+    }
+    void tuneChannel() override {
+        std::cout << "Switching TCL TV channel" << std::endl;
+    }
+};
+
+class RemoteControl {
+public:
+    RemoteControl(std::shared_ptr<TV> tv) : tv(tv) {}
+    void on() {
+        tv->on();
+    }
+    void off() {
+        tv->off();
+    }
+    void tuneChannel() {
+        tv->tuneChannel();
+    }
+private:
+    std::shared_ptr<TV> tv;
+};
+
+int main() {
+    int n;
+    int tv_brand, tv_operation;
+
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> tv_brand >> tv_operation;
+
+        std::shared_ptr<TV> tv;
+        if (tv_brand == 0) {
+            tv = std::make_shared<SonyTV>();
+        } else if (tv_brand == 1) {
+            tv = std::make_shared<TCLTV>();
+        } else {
+            std::cout << "Invalid TV brand" << std::endl;
+            continue;  
+        }
+
+        RemoteControl remote(tv);
+
+        if (tv_operation == 2) {
+            remote.on();
+        } else if (tv_operation == 3) {
+            remote.off();
+        } else if (tv_operation == 4) {
+            remote.tuneChannel();
+        } else {
+            std::cout << "Invalid operation" << std::endl;
+        }
+    }
+}
---
+++ 10_Bridge/easy_solution.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+int main() {
+    int n;
+    int tv_brand;
+    int tv_operation; 
+
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> tv_brand >> tv_operation;
+        if (tv_brand == 0) {
+            if (tv_operation == 2) {
+                std::cout << "Sony TV is ON" << std::endl;
+            } else if (tv_operation == 3) {
+                std::cout << "Sony TV is OFF" << std::endl;
+            } else if (tv_operation == 4) {
+                std::cout << "Switching Sony TV channel" << std::endl; 
+            } else {
+                std::cout << "Invalid operation" << std::endl;
+            }
+        } else if (tv_brand == 1) {
+            if (tv_operation == 2) {
+                std::cout << "TCL TV is ON" << std::endl;
+            } else if (tv_operation == 3) {
+                std::cout << "TCL TV is OFF" << std::endl;
+            } else if (tv_operation == 4) {
+                std::cout << "Switching TCL TV channel" << std::endl; 
+            } else {
+                std::cout << "Invalid operation" << std::endl;
+            }
+        } else {
+            std::cout << "Invalid TV brand" << std::endl;
+        }
+    }
+}
