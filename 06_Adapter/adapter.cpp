800fdc340eaff6bcb91e55a5e3ffcca8c914f513
andy-lee-cat <2048513251@qq.com>
2024-12-24 19:50:09 +0800

    Add 06_adapter

---
+++ 06_Adapter/adapter.cpp
@@ -0,0 +1,72 @@
+// adapter
+#include<iostream>
+#include<unordered_map>
+#include<memory>
+
+class USB {
+public:
+    void charge() {
+        // do nothing
+    }
+};
+
+class TypeC {
+public:
+    void charge() {
+        std::cout << "TypeC" << std::endl;
+    }
+};
+
+class USBAdapter {
+public:
+    USBAdapter(std::shared_ptr<USB> usb) : usb(usb) {}
+    void charge() {
+        std::cout << "USB Adapter" << std::endl;
+        usb->charge(); 
+    }
+private:
+    std::shared_ptr<USB> usb;
+};
+
+class Computer {
+public:
+    Computer(std::shared_ptr<USBAdapter> adapter) : adapter(adapter) {
+        typeC = std::make_shared<TypeC>();
+    }
+    void chargeWithTypeC() {
+        typeC->charge();
+    }
+    void chargeWithAdapter() {
+        adapter->charge();
+    }
+private:
+    std::shared_ptr<TypeC> typeC;  
+    std::shared_ptr<USBAdapter> adapter;  
+};
+
+
+int main() {
+    int n;
+    int option;
+
+    // I have a USB
+    std::shared_ptr<USB> usb = std::make_shared<USB>();
+
+    // I have a USB adapter
+    std::shared_ptr<USBAdapter> adapter = std::make_shared<USBAdapter>(usb);
+
+    // I have a computer with a USB adapter
+    std::shared_ptr<Computer> computer = std::make_shared<Computer>(adapter);
+
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> option;
+        if (option == 1) {
+            computer->chargeWithTypeC();
+        } else if (option == 2) {
+            computer->chargeWithAdapter();
+        } else {
+            std::cout << "Invalid option" << std::endl;
+        }
+    }
+}
---
+++ 06_Adapter/easy_solution.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<unordered_map>
+
+int main() {
+    int n;
+    int type_name;
+    std::unordered_map<int, std::string> map = {
+        {1, "TypeC"},
+        {2, "USB Adapter"}
+    };
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> type_name;
+        auto it = map.find(type_name);
+        if (it != map.end()) {
+            std::cout << it->second << std::endl;
+        } else {
+            std::cout << "Unknown type" << std::endl;
+        }
+    }
+}
